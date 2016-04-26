a3ce67443c28489fa2188e72e45ac5a99b4d0d80
waynecz <451578533@qq.com>
2016-04-26 13:58:13 +0800

    init

---
+++ assets/js/hello.js
@@ -0,0 +1,11 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component{
+    render(){
+        return <div>
+            hello
+        </div>
+    }
+}
+ReactDOM.render(<App/>, document.getElementById('APP'));
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+var ASS = path.resolve(__dirname, 'assets');
+var BUILD = path.resolve(__dirname, 'build');
+
+
+module.exports = {
+    entry: {
+        app: ['webpack/hot/dev-server', 'webpack-dev-server/client?http://127.0.0.1:8080', './assets/main.js']
+    },
+    output: {
+        path: BUILD,
+        filename: '[name].js',
+        publicPath: "http://127.0.0.1:8080/build"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js[x]?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react','es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+
+    ]
+};
